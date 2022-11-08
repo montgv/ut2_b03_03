982a9d156a7958bb1a3f1bbf9ae66cb577df9e1c
estudiante <montserrat.garrido.220@estudiante.iesoretania.es>
2022-11-08 13:28:13 +0100

    Commit terminado y comentado

--- core/src/main/java/es/iesoretania/dam2/hlc/Meteorito.java
+++ core/src/main/java/es/iesoretania/dam2/hlc/Meteorito.java
@@ -13,15 +13,15 @@ public class Meteorito extends Actor {
     private TextureRegion regionActual;
     private int numeroMeteorito;
 
+    //Cargamos las diferentes texturas de los diferentes meteoritos y le asignamos a regionActual un meteorito
     public Meteorito(float x, float y) {
         Texture completo = new Texture(Gdx.files.internal("spacetheme.png"));
-        meteorito[0] = new TextureRegion(completo, 0, 300, 55, 63);
-        meteorito[1] = new TextureRegion(completo, 100, 270, 20, 20);
-        meteorito[2] = new TextureRegion(completo, 65, 250, 35, 55);
-        meteorito[3] = new TextureRegion(completo, 65, 300, 25, 45);
-        meteorito[4] = new TextureRegion(completo, 100, 270, 20, 20);
-        meteorito[5] = new TextureRegion(completo, 90, 300, 25, 30);
-        numeroMeteorito = MathUtils.random(0, 5);
+        meteorito[0] = new TextureRegion(completo, 3, 246, 54, 52);
+        meteorito[1] = new TextureRegion(completo, 64, 262, 29, 30);
+        meteorito[2] = new TextureRegion(completo, 107, 270, 11, 11);
+        meteorito[3] = new TextureRegion(completo, 5, 306, 47, 41);
+        meteorito[4] = new TextureRegion(completo, 63, 315, 17, 25);
+        meteorito[5] = new TextureRegion(completo, 94, 316, 20, 20);
         regionActual = meteorito[0];
     }
 
@@ -29,16 +29,18 @@ public class Meteorito extends Actor {
         this(400, 240);
     }
 
+    //Dibujamos el meteorito
     @Override
     public void draw(Batch batch, float parentAlpha) {
         batch.draw(regionActual, this.getX(), this.getY(), this.getOriginX(), this.getOriginY(),
                 this.getWidth(), this.getHeight(), this.getScaleX(), this.getScaleY(), this.getRotation());
     }
 
+    //Indicamos al meteorito que es lo que tiene que hacer
     @Override
     public void act(float delta) {
         this.moveBy(0, -250 * Gdx.graphics.getDeltaTime());
-        this.rotateBy(180 * Gdx.graphics.getDeltaTime() / 2);
+        this.rotateBy(360 * Gdx.graphics.getDeltaTime() / 2);
         if (getY() < 0 - getHeight()) {
             numeroMeteorito = MathUtils.random(0, 5);
 
@@ -69,6 +71,7 @@ public class Meteorito extends Actor {
         }
     }
 
+    //Funcion que genera los meteoritos en la parte de arriba
     public void generarMeteorito() {
         setSize(regionActual.getRegionWidth(), regionActual.getRegionHeight());
         setPosition(MathUtils.random(0, 640 - regionActual.getRegionWidth()), 480);
